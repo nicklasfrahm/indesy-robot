3d39ac6b52e00191cb5cb0252a842bd39e3922a9
electron-byte <nicklas.frahm@gmail.com>
2017-10-26 18:58:44 +0200

    Add smarter error handling

--- source/commands.js
+++ source/commands.js
@@ -1,8 +1,15 @@
 const { exec } = require('child_process')
+const winston = require('winston')
+
+winston.cli()
+
+function reportError(cb) {
+  return (err, stdout, stderr) => {
+    if (err) winston.error(`${err.message}\n${stdout}\n${stderr}`)
+    if (cb && typeof cb === 'function') return cb()
+  }
+}
 
 exports.pullRepo = cb => {
-  exec('git pull', (err, stdout, stderr) => {
-    if (err) return console.log(err)
-    if (cb && typeof cb === 'function') return cb()
-  })
+  exec('git pull', reportError(cb))
 }
