6ae387852442af176b3ae9b111dde90e50b13c50
Nicklas Frahm <u330510@danfoss.com>
2017-12-13 20:54:56 +0100

    Add loop function

--- source/motion.js
+++ source/motion.js
@@ -4,9 +4,18 @@ const os = require('os').platform()
 const sensor = require('./LSM6DS3')
 
 const deviceAddress = 0x6a
+let readWord = null
 let bus = null
 
 function initializeSensor(cb) {
+  readWordLogLoop = (register, message) => {
+    bus.readWord(deviceAddress, register, (err, word) => {
+      if (err) throw err
+      process.stdout.write(`${message}${word}\n`)
+      readWordLogLoop(register)
+    })
+  }
+
   bus.writeByte(deviceAddress, sensor.CTRL9_XL, 0x38, err => {
     if (err) return cb(err)
     bus.writeByte(deviceAddress, sensor.CTRL1_XL, 0x60, err => {
@@ -26,9 +35,10 @@ bus = i2c.open(1, function(err) {
   if (err) throw err
   initializeSensor(err => {
     if (err) throw err
-    bus.readWord(deviceAddress, sensor.OUT_TEMP_L, (err, temp) => {
-      if (err) throw err
-      process.stdout.write(`${temp}\n`)
-    })
+
+    // read temperature as test
+    readWordLogLoop(sensor.OUTX_L_XL, 'X: ')
+    readWordLogLoop(sensor.OUTY_L_XL, 'Y: ')
+    readWordLogLoop(sensor.OUTZ_L_XL, 'Z: ')
   })
 })
